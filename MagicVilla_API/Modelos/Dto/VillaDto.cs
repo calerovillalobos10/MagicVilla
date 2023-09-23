6c17aeeca082e18b3d9c3de6b13a39cc71b05cdf
Thomas <calerovillalobos10@gmail.com>
2023-09-23 15:13:32 -0600

    Agrega el API PUT UpdateVIlla
    Se introduce el enpoint que permite actualizar una villa completamentre

--- MagicVilla_API/Modelos/Dto/VillaDto.cs
+++ MagicVilla_API/Modelos/Dto/VillaDto.cs
@@ -8,5 +8,8 @@ namespace MagicVilla_API.Modelos.Dto
         [Required] //Hace que el parámetro sea requerido
         [MaxLength(30)] //Hace que tenga un máximo que uno define
         public string Nombre { get; set; }
+
+        public int Ocupantes { get; set; }
+        public int MetrosCuadrados { get; set; }
     }
 }
